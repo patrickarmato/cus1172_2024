709d11dc1a03b684ebcada1bce223d27076b0a32
patrickarmato <patrick.armato20@my.stjohns.edu>
2024-04-03 04:28:26 -0400

    commit

---
+++ Project3/project3.js
@@ -0,0 +1,42 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    widget_html = render_view("#quiz_selection");
+
+    document.querySelector("#app_widget").innerHTML = widget_html;
+
+    create_user_view(1)
+
+    document.querySelector("#quiz1").onclick = function(){
+        widget_html = render_view("#question");
+        document.querySelector("#app_widget").innerHTML = widget_html;
+    }
+
+})
+
+const create_user_view = async (user_idx) => {
+
+    const data = await fetch("https://")
+    const model = await data.json
+    const html_element = render_widget(model, '#user_view')
+    document.querySelector('#app_widget').innerHTML = html_element;
+
+}
+
+const render_view = (view, model) => {
+
+    template_source = document.querySelector(view).innerHTML
+
+    var template = Handlebars.compile(template_source);
+
+    /*var model = {
+        question : 'How are you?'
+    }*/
+
+    console.log(model)
+
+    var html_widget_element = template(model)
+    console.log(html_widget_element)
+    return html_widget_element
+}
